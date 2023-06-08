e1c69a7f2128dc3588b81eaeea869ad12aa02180
Ean Hatfield <hatfield.69@wright.edu>
2023-06-08 17:36:02 -0400

    Removed redundant object

--- src/src/MudScript.java
+++ src/src/MudScript.java
@@ -5,10 +5,5 @@ import javax.script.ScriptException;
 public class MudScript {
     public static void main(String[] args) {
         MainFrame frame = new MainFrame();
-        ScriptEngineManager manager = new ScriptEngineManager();
-
-        ScriptEngine engine = manager.getEngineByName("JavaScript");
-
-
     }
 }
