109566de2899d42ae5113e4f914155ffef23221d
Ean Hatfield <89809693+ReeseHatfield@users.noreply.github.com>
2023-06-22 13:30:19 -0400

    Added documentation

--- src/src/MainFrame.java
+++ src/src/MainFrame.java
@@ -13,20 +13,29 @@ public class MainFrame extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500, 500);
         this.setLayout(new BorderLayout());
+        //frame creation
 
+        //initialize fields
         this.textArea = new JTextArea();
 
         JScrollPane scrollPane = new JScrollPane(textArea);
-        this.add(scrollPane, BorderLayout.CENTER); // Center will occupy the majority of space
+        this.add(scrollPane, BorderLayout.CENTER); 
+        // center will occupy the majority of space
 
+        //initialize compile button
         this.compileBtn = new JButton("Compile");
+        
         this.compileBtn.addActionListener(e -> {
+
+            //compile button action
             String code = this.textArea.getText();
 
             ScriptEngineManager manager = new ScriptEngineManager();
+            //add text field to script managers engine
 
             ScriptEngine engine = manager.getEngineByName("JavaScript");
 
+            //Attempt to evaluate the code provided
             try{
                 engine.eval(code);
             } catch (ScriptException scriptException) {
@@ -34,13 +43,14 @@ public class MainFrame extends JFrame {
             }
         });
 
-
+        //Add buttons to panel and panel to root
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new GridBagLayout());
         buttonPanel.add(this.compileBtn);
 
         this.add(buttonPanel, BorderLayout.SOUTH);
-
+        
+        //set all elements to be visible
         this.setVisible(true);
     }
 }
